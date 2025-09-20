018ab638787d74bc513b8402ef936c411cd15212
raghulmanik <raghulmanik.dev@gmail.com>
2025-09-20 17:29:41 +0530

    add try/catch block with checkXandY & divide rule

--- calculator.js
+++ calculator.js
@@ -1,27 +1,60 @@
 const prompt = require("prompt-sync")();
 
-let operations = ['+',`-`,`*`,`/`]
+let operations = ['+',`-`,`*`,`/`];
 
-let x = Number(prompt('Enter Your first number: '));
-let y = Number(prompt('Enter Your Second number: '));
 
-console.log(`From the listed operation enter their index to perform that operation\n1. add\n2. sub\n3. mul\n4. div`);
-let operator = Number(prompt("Enter the listed operations to perform: "))
+function checkXandY(x,y){
+    let whichVar;
 
-let choice = operations[--operator];
-let ans;
-switch (choice){
-    case "+":
-        ans = x + y;
-        break;
-    case "-":
-        ans = x - y;
-        break;
-    case "*":
-        ans = x * y;
-        break;
-    case "/":
-        ans = x / y;
-        break;
+    if((isNaN(x) && (whichVar = 'X')) || (isNaN(y) && (whichVar = 'Y'))){
+        throw new Error(`${whichVar} is not a Number`);
+    }
 }
-console.log(`Answer: ${x} ${choice} ${y} = ${ans}`);
+
+function Divide(x,y){
+    if(y === 0){
+        throw new Error(`In Division "Y" cannot be 0`)
+    }
+
+    return x / y;
+}
+
+
+try{
+
+    let x = Number(prompt('Enter Your first number: '));
+    let y = Number(prompt('Enter Your Second number: '));
+
+    checkXandY(x, y);
+
+    console.log(`From the listed operation enter their index to perform that operation\n1. add\n2. sub\n3. mul\n4. div`);
+
+    let operator = Number(prompt("Enter the listed operations to perform: "))
+
+    let choice = operations[--operator];
+    let ans;
+    switch (choice){
+        case "+":
+            ans = x + y;
+            break;
+        case "-":
+            ans = x - y;
+            break;
+        case "*":
+            ans = x * y;
+            break;
+        case "/":
+            ans = Divide(x,y);
+            break;
+        default:
+            throw new Error("Invalid Operator Choice");
+    }
+
+    console.log(`Answer: ${x} ${choice} ${y} = ${ans}`);
+
+}catch(err){
+    console.error(err.toString())
+}
+finally{
+    console.log("Calculator has ended................")
+}
